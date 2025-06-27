ea816cbaf974a04c6a98b14efb4a19dac91a6693
BunnyeNyash <125456472+BunnyeNyash@users.noreply.github.com>
2025-06-27 23:24:31 +0300

    A Python script to create the alx_book_store database in MySQL, ensuring it doesn’t fail if the database already exists, and print appropriate success or error messages.

--- MySQLServer.py
+++ MySQLServer.py
@@ -15,7 +15,7 @@ try:
     mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
     print("Database 'alx_book_store' created successfully!")
     
-except Error as err:
+except mysql.connector.Error as err:
     print(f"Error: {err}")
     
 finally:
@@ -23,3 +23,4 @@ finally:
         mycursor.close()
     if mydb:
         mydb.close()
+        
